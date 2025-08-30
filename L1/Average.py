c462651502d4a7003952b8abcffa0a09bd7435fd
fairyaisha14 <aishaawoniyi@gmail.com>
2025-08-30 16:59:47 +0100

    average.commit

---
+++ L1/Average.py
@@ -0,0 +1,16 @@
+# There are five trees in Jack's front yard. He checks each tree to find out how tall it is in inches and writes the height on a sheet of paper. Jack's list: 98, 94, 41, 96, and 11. What is the average height of a tree in Jack's front yard?
+
+# Storing Values
+tree1 = 98
+tree2 = 45
+tree3 = 64
+tree4 = 53
+tree5 = 165
+
+# Finding the total of trees 
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("The sum of all 5 trees is: ", sum)
+
+# Finding the average of trees
+average = sum/5
+print("The average of all the trees are:", average)
