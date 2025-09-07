2d69ad4928e32dcc589ad265e4b981b42427d11c
fairyaisha14 <aishaawoniyi@gmail.com>
2025-09-07 16:46:21 +0100

    AND-OR operator.commit

--- L3/AND OR operator.py
+++ L3/AND OR operator.py
@@ -1,2 +1,10 @@
 #Write a program to check whether the given values have boolean values or not.
-a
+a = 23
+b = 24
+c = 22
+
+
+if a > b or a < c:
+    print("I am using or operator")
+elif a < b and a >c:
+    print("I am using and operator")
