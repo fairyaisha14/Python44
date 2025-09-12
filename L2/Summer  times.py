0829c6d5f0d8da30b9a275fd59832235ec678911
fairyaisha14 <aishaawoniyi@gmail.com>
2025-09-12 16:44:06 +0100

    summer.commit

---
+++ L2/Summer  times.py
@@ -0,0 +1,6 @@
+temp = float(input("What is the temperature at your location? "))
+
+if temp < 16:
+    print("You should wear a jacket.")
+else:
+    print("You can wear either a cardigan or normal clothes.")
