14d1292901a69c7c6df29238ae6e343dbc35c695
fairyaisha14 <aishaawoniyi@gmail.com>
2025-09-14 14:52:29 +0100

    alphabet.commit

---
+++ L3/Checking alphabets.py
@@ -0,0 +1,9 @@
+letter = input("Tell me a letter of the alphabet and I will tell you if it is true or not: ").lower()
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+if letter in alphabet:
+    print("True")
+else:
+    print("False")
+
+  
