51df837683ef13e2dacd79fa4ee366a9d97500ee
fairyaisha14 <aishaawoniyi@gmail.com>
2025-09-28 16:49:07 +0100

    natral numbers.commit

---
+++ L11/Sum of Natural numbers.py
@@ -0,0 +1,11 @@
+#Input the value of terms
+n=int(input("Enter the value of terms"))
+
+sum = 1 #initialise
+i = 1 # inisialise
+while i<=n: #loop will run from 1 to n
+    sum = sum+i 
+    i= i+1
+
+print("\nSum = ", sum)
+ 
