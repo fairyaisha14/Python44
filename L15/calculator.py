90b7dfe42b03eb810e1db899c18e9ccabe694788
fairyaisha14 <aishaawoniyi@gmail.com>
2025-10-12 17:18:15 +0100

    hi.commit

--- L15/calculator.py
+++ L15/calculator.py
@@ -1,27 +1,30 @@
-#Write a program to make a calculator : For making a calculator create four functions add, subtract, multiply, divide. Ask for a choice from users which operation they want to perform. Take user input whatever operation they want to perform And call that function accordingly
-def add (P, Q):
-    #This function is used for adding two numbers
-    return P + Q
-def subtarct(P, Q):
-    #This function is used for subtracting two numbers
+def add(P, Q):
+    return P+Q
+def subtract(P, Q):
     return P - Q
 def multiply(P, Q):
-    #This function is used for multiplying two numbers
     return P * Q
-def divide (P, Q):
-    #This function is used for dividing two numbers
+
+def divide(P, Q):
     return P / Q
-#Now we will take input from the user 
-print ("Please select the operation.")
+print("Please select operation.")
 print("a. Add")
 print("b. Subtract")
 print("c. Multiply")
 print("d. Divide")
+
 choice = input("Please enter choice (a/b/c/d):")
 num_1 = int(input("Please enter the first number: "))
-num_2 = int(input("Please enter the second number: ")) 
-
-if choice =='a':
-    print(num_1, "+", num_2)
+num_2 = int(input("Please enter the second number: "))
+if choice == 'a':
+    print(num_1, "+", num_2, "=", add(num_1, num_2))
+elif choice == 'b':
+    print(num_1, "-", num_2, "=", subtract(num_1, num_2))
+elif choice == 'c':
+    print(num_1, "*", num_2, "=", multiply(num_1, num_2))
+elif choice == 'd':
+    print(num_1, "/", num_2, "=", divide(num_1, num_2))
+else:
+    print("This is an invalid input")
 
 
