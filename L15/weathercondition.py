f4c40487060353c6cd26e9afd2fae20520f32f6e
fairyaisha14 <aishaawoniyi@gmail.com>
2025-10-12 16:51:57 +0100

    autumn.commit

---
+++ L15/weathercondition.py
@@ -0,0 +1,9 @@
+#Write a program to display the weather in autumn and spring
+def weather_condition():
+    print("The weather is pleasant in:", spring)
+    print('The weather is same in', autumn)
+
+
+spring = 'autumn'
+autumn = spring
+weather_condition()
