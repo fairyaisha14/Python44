080f92e2777181314c159c7853539e195af33f15
fairyaisha14 <aishaawoniyi@gmail.com>
2025-10-12 15:38:38 +0100

    total.commit

---
+++ L11/Totaldigits.py
@@ -0,0 +1,17 @@
+num = int(input("Write down any number: "))
+while num < 10:
+    print ("This number has 1 digit")
+while num < 100:
+    print ("This number has 2 digits")
+while num < 1000:
+    print ("This number has 3 digits")
+while num < 10000:
+    print ("This number has 4 digits")
+while num < 100000:
+    print ("This number has 5 digits")
+while num < 1000000:
+    print ("This number has 6 digits")
+while num < 10000000:
+    print ("This number has 7 digits")
+else:
+    print ("This numbers has more than 7 digits")
