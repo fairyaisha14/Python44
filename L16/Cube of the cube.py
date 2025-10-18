11666d2d821fc75cf1d1570523b4f2969096f984
fairyaisha14 <aishaawoniyi@gmail.com>
2025-10-18 17:00:07 +0100

    cube.commit

---
+++ L16/Cube of the cube.py
@@ -0,0 +1,6 @@
+#Define a function to find a cube and define another function which let execute the cube function if the number is divisible by 3
+def cube(num):
+    return num*num*num
+
+num = 5
+print (f"The cube of {num} is {cube(num)}")
