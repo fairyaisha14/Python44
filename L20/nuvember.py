649c2adaffddff83151bb561c4cdfd07a639c67c
fairyaisha14 <aishaawoniyi@gmail.com>
2025-11-01 16:08:35 +0000

    random dates.py

---
+++ L20/nuvember.py
@@ -0,0 +1,19 @@
+#Write a program to check the current date and time?
+import random
+
+import time
+
+
+def getRandomDate(starDate, endDate):
+    print("Printing random date between", starDate, "and", endDate)
+    randomGenerator = random.random()
+    dateFormat = '%m/%d/%Y'
+    starttime = time.mktime(time.strptime(starDate, dateFormat ))
+    endtime = starttime = time.mktime(time.strptime(endDate, dateFormat ))
+    randomTime = starttime + randomGenerator* (endtime - starttime)
+    randomDate = time.strftime(dateFormat, time.localtime(randomTime))
+    return randomDate
+
+
+print("Random Date = ", getRandomDate("1/1/2020", "12/12/2024" ))
+
