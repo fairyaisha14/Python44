2743014265fddc0414db49108f6b5d8189de4c38
fairyaisha14 <aishaawoniyi@gmail.com>
2025-11-02 15:52:26 +0000

    list.commit

---
+++ L21/reverse.py
@@ -0,0 +1,20 @@
+#Write a program to perform the following operations on a List: 1. Create an empty list 2. A list with elements 3. Use * operator 4. Reverse a list
+#Create a empty list
+empty_list = []
+print()
+
+# A list of numbers
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+#Use * operator
+triples = [1,2,3] * 3
+print(triples)
+
+#reverse the given list
+aList = [10, 200, 300, 400, 500] 
+aList = aList[::-1]
+print(aList,"\n")
+
+
+
