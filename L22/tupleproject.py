fe2154144653710a7ef5bc1f594488f8938a9987
fairyaisha14 <aishaawoniyi@gmail.com>
2025-11-22 14:51:48 +0000

    numbers.commit

---
+++ L22/tupleproject.py
@@ -0,0 +1,9 @@
+tuple1 = (4, 3, 2, 2, -1, 18)
+print(tuple1)
+pro = 4*3*2*2*-1*18
+print ("The product of these numbers are", pro)
+
+tuple2 = (2, 4, 8, 8, 3, 2, 9)
+print(tuple2)
+pro = 2*4*8*8*3*2*9
+print ("The product of these numbers are", pro)
