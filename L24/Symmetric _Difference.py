0d6c48279144aa0386fc26ffa29ee064632bc014
fairyaisha14 <aishaawoniyi@gmail.com>
2025-12-13 15:23:16 +0000

    symmetric.commit

---
+++ L24/Symmetric _Difference.py
@@ -0,0 +1,12 @@
+Set1 = {'blue', 'green'}
+Set2 = {'blue', 'yellow'}
+
+symmetric_diff = Set1.symmetric_difference(Set2)
+print(symmetric_diff)
+
+
+Set1 = {1, 2, 3, 4, 5}
+Set2 = {1, 5, 6, 7, 8, 9 }
+
+symmetric_diff = Set1.symmetric_difference(Set2)
+print(symmetric_diff)
