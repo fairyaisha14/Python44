9a48e6ce0761329c8ee31ec86817c64f74a5a877
fairyaisha14 <aishaawoniyi@gmail.com>
2025-12-13 17:52:17 +0000

    student.commit

---
+++ L27/student.py
@@ -0,0 +1,9 @@
+#Write a program to create a class with the name Student and perform the following tasks - 1. Declare a variable grade 2. Print a sentence inside the class 3. Create an object of class student and see the output
+# create a class
+class student:
+    grade = 10
+    print("Hi I am a student of grade", grade)
+
+
+# create an object
+ob = student()
