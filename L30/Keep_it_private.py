99a7f359ab6783a5ae5496f36bdb412df4845b9e
fairyaisha14 <aishaawoniyi@gmail.com>
2025-12-21 15:56:25 +0000

    hi.commit

--- L30/Keep_it_private.py
+++ L30/Keep_it_private.py
@@ -9,7 +9,7 @@ class myClass:
     #private method
     def __privMeth(self):
         print("I'm inside class myClass")
-
+    
     #Function to print value of private variable
     def hello(self):
         print("Private Variable value: ",myClass.__privateVar)
